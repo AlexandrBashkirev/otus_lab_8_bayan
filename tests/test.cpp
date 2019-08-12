0526a19c7047672c2a22ec57bf110655bdb5d1af
AlexandrBashkirev <alexandr.bashkirev@gmail.com>
2019-08-12 17:13:22 +0300

    -init

---
+++ tests/test.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE test
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(nickname_test_suite)
+
+BOOST_AUTO_TEST_CASE(test)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
