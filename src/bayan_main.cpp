e923323fc89ad477f7b3e81237305161ca1080cb
AlexandrBashkirev <alexandr.bashkirev@gmail.com>
2019-08-13 08:50:56 +0300

    -boost build test

--- src/bayan_main.cpp
+++ src/bayan_main.cpp
@@ -2,8 +2,23 @@
 //
 
 #include <iostream>
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
 
 int main()
 {
-	
+	path p{ "C:\\Windows\\System" };
+
+#ifdef BOOST_WINDOWS_API
+	std::wcout << p.native() << '\n';
+#else
+	std::cout << p.native() << '\n';
+#endif
+
+	std::cout << p.string() << '\n';
+	std::wcout << p.wstring() << '\n';
+
+	std::cout << p.generic_string() << '\n';
+	std::wcout << p.generic_wstring() << '\n';
 }
